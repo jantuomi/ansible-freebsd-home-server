017b29539749da3c62202879d757a45ffc71208c
Jan Tuomi <jan@jantuomi.fi>
2026-08-03 22:27:15 +0300

    Improvements

--- roles/jails/16_dl/files/fix-library-permissions.sh
+++ roles/jails/16_dl/files/fix-library-permissions.sh
@@ -6,5 +6,5 @@ DIRS="/mnt/storage/media /mnt/storage/downloads"
 
 for d in $DIRS; do
         chown -R storage:storage $d
-        chmod -R u=rwX,g=rX,o=rX $d
+        chmod -R u=rwX,g=rwX,o=rX $d
 done
