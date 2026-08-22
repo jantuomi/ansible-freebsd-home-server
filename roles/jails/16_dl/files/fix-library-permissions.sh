30f599e897e2507ee33647d2eaa04f173a91c953
Jan Sydänviita <jan@sydanviita.fi>
2026-08-22 14:57:30 +0300

    Update fix-library-permissions.sh

--- roles/jails/16_dl/files/fix-library-permissions.sh
+++ roles/jails/16_dl/files/fix-library-permissions.sh
@@ -5,6 +5,6 @@ set -eu
 DIRS="/mnt/storage/media /mnt/storage/downloads"
 
 for d in $DIRS; do
-        chown -R storage:storage $d
+        chgrp -R storage $d
         chmod -R u=rwX,g=rwX,o=rX $d
 done
